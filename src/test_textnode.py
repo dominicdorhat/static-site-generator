c34918d0002d65e08d612789cd24245cc9faf998
dominicdorhat <johnfruit099@gmail.com>
2024-04-22 14:52:25 +0800

    Added more unit tests for TextNode [Ch2,2]

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,19 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
 
-            
+    def test_noteq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("node", "bold")
+        self.assertNotEqual(node, node2)
+
+    def test_urlNone(self):
+        node = TextNode("This is a text node", "bold")
+        self.assertIsNone(node.url)
+
+    def test_texttype_noteq(self):
+        node = TextNode("This is a text node", "italics")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertNotEqual(node.text_type, node2.text_type)
+
     if __name__ == "__main__":
         unittest.main()
