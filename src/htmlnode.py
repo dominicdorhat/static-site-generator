5a336b66579902e7fda140c5122bf50b6fce3048
dominicdorhat <johnfruit099@gmail.com>
2024-04-29 13:55:33 +0800

    Created ParentNode with unit tests + renamed variables in HTMLNode. [Ch2,5]

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -12,23 +12,23 @@ class HTMLNode:
         #   2. derived classes MUST override this method
         raise NotImplementedError()
 
-    # return string representation of the HTML attributes of node
     def props_to_html(self):
-        text = ""
+        props = ""
         if not self.props:
-            return text
+            return props
         else:
             for key, value in self.props.items():
-                text += f" {key}=\"{value}\""
+                props += f" {key}=\"{value}\""
 
         # remove last space
-        # return text[:-1]
-        return text.rstrip()
+        return props.rstrip()
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
+
 class LeafNode(HTMLNode):
+
     def __init__(self,tag, value, props=None):
         super().__init__(tag, value, None, props)
 
@@ -45,4 +45,29 @@ class LeafNode(HTMLNode):
         return html_string
 
     def __repr__(self):
-        return f"LeafNode({self.tag},{self.value},{self.props})"
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
+
+
+class ParentNode(HTMLNode):
+
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Invalid HTML tag.")
+
+        if not self.children:
+            raise ValueError(f"{self.tag} has no valid children.")
+
+        # base case: LeafNode
+        # ex:  <body><p><b>Losum epsum</b></p></body>
+        html = f"<{self.tag}{self.props_to_html()}>"
+        for child in self.children:
+            html += f"{child.to_html()}"
+
+        html += f"</{self.tag}>"
+        return html
+
+    def __repr__(self):
+        return f"ParentNode({self.tag}, {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
 
@@ -7,23 +7,18 @@ class TestHTMLNode(unittest.TestCase):
     nodeh1 = HTMLNode("h1", "README")
     nodeLink = HTMLNode("a", "This is a link", props = {"href": "https://www.boot.dev"})
     nodeLink2 = HTMLNode("a", "This is a 2nd link", props = {"href": "https://www.boot.dev", "target": "_blank"})
-    nodeEmptyP = HTMLNode("p")
-    nodeRawText = HTMLNode(value="Raw Text")
+    nodeRawText = LeafNode(None, value="Raw Text")
     nodeEmptyNode = HTMLNode()
 
-    node_children = [nodeh1, nodeLink, nodeEmptyP]
-
-    nodeHTML = HTMLNode("html",children = node_children)
-
     ## [LeafNodes]
-    nodeRawTextLN = LeafNode(None, "Raw Text")
     nodeh1LN = LeafNode("h1", "README")
     nodeLinkLN = LeafNode("a", "Click me!", props={"href": "www.boot.dev"})
     nodeLink2LN = LeafNode("a", "Click me too!", props={"href": "www.boot.dev", "target": "_blank"})
+    nodeRawTextLN = LeafNode(None, "Raw Text")
 
-#   How to test for Exceptions? Unless you don't.
-#    def test_htmlException(self):
-#       self.assertIs(self.nodeh1.to_html(), NotImplementedError())
+    ## [Parent Nodes]
+    nodeP = ParentNode("p", [LeafNode("b", "Lorem epsum")])
+    nodeBody = ParentNode("body", children = [nodeh1LN, nodeP])
 
     def test_props_to_html(self):
         self.assertEqual(self.nodeLink.props_to_html(), " href=\"https://www.boot.dev\"")
@@ -47,10 +42,29 @@ class TestHTMLNode(unittest.TestCase):
     def test_h1LN(self):
         self.assertEqual(self.nodeh1LN.to_html(), "<h1>README</h1>")
 
-    def test_link_props_to_html(self):
+    def test_link_to_html(self):
         self.assertEqual(self.nodeLinkLN.to_html(), "<a href=\"www.boot.dev\">Click me!</a>")
         self.assertEqual(self.nodeLink2LN.to_html(), "<a href=\"www.boot.dev\" target=\"_blank\">Click me too!</a>")
 
+    def test_render_to_html(self):
+        node = LeafNode("a", "link")
+        self.assertEqual(self.nodeBody.to_html(), "<body><h1>README</h1><p><b>Lorem epsum</b></p></body>")
+
+    def test_nested_parents_to_html(self):
+        nodeLink = LeafNode("a", "Click me too!", props={"href": "www.boot.dev", "target": "_blank"})
+        node = ParentNode("html", [
+                ParentNode("head", [LeafNode("h1", "HEADER"), nodeLink]),
+                ParentNode("body", [ParentNode("p", [LeafNode("i", "Lorem Ipsum"), LeafNode(None, "raw text")])])])
+
+        self.assertEqual(node.to_html(), "<html><head><h1>HEADER</h1><a href=\"www.boot.dev\" target=\"_blank\">Click me too!</a></head><body><p><i>Lorem Ipsum</i>raw text</p></body></html>")
+
+    def test_linear_to_html(self):
+        # multiple levels deep but 1 child per level
+
+        node = ParentNode("html", [ParentNode("body", [ParentNode("p", [ParentNode("b", [LeafNode("i", "Hello")])])])])
+        print(node)
+
+        self.assertEqual(node.to_html(), "<html><body><p><b><i>Hello</i></b></p></body></html>")
 
     if __name__ == "__main__":
         unittest.main()
